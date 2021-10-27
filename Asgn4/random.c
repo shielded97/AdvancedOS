ffcbdd3de4c082e2225b542e1ad037316b037cca
shielded97 <49818031+shielded97@users.noreply.github.com>
2021-10-27 18:08:20 -0500

    Add files via upload

---
+++ Asgn4/random.c
@@ -0,0 +1,14 @@
+  //struct dirStruct *dir = mkDirStruct(0, directory);
+  //writeDirStruct(dir, 17, directory);
+  //fprintf(stdout,"cpmRename return code = %d,\n\n",cpmRename("mytestf1.txt", "short.fps")); 
+  //struct dirStruct *dir1 = mkDirStruct(0, directory);
+  //fprintf(stdout,"cpmRename return code = %d,\n",cpmRename("mytestf1.txt", "shortf.ps")); 
+
+    // int j = findExtentWithName("mytestf1.txt", directory);
+  // int k = findExtentWithName("-----", directory);
+  // int l = findExtentWithName("test.---", directory);
+  // int m = findExtentWithName("test.jiji", directory);
+  // int n = findExtentWithName("testteste.k", directory);
+  // int o = findExtentWithName("test", directory);
+  // int p = findExtentWithName("shortf.ps", directory);
+  // printf("Index of mytestf1.txt: %d\n", j);
